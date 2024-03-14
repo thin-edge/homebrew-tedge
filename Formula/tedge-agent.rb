a67495d1c5820ca8dc1ee474bd16031835608d2b
Reuben Miller <reuben.d.miller@gmail.com>
2024-03-14 21:02:37 +0100

    add empty install method

--- Formula/tedge-agent.rb
+++ Formula/tedge-agent.rb
@@ -5,10 +5,18 @@ class TedgeAgent < Formula
     license "Apache-2.0"
     url "https://thin-edge.io/"
     depends_on "tedge" => :optional
+    def install
+        # Do nothing
+    end
     service do
         name macos: "tedge-agent",
             linux: "tedge-agent"
         run ["#{HOMEBREW_PREFIX}/bin/tedge-agent", "--config-dir", etc/"tedge"]
         keep_alive false
+    end
+    def caveats
+        <<~EOS
+            Install tedge-agent service
+        EOS
     end
 end
