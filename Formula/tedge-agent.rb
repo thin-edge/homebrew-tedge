82576919ac1e24b420a7707af28cf9bb4cab92aa
Reuben Miller <reuben.d.miller@gmail.com>
2024-09-16 11:18:48 +0200

    feat: add logrotate definitions for tedge services

--- Formula/tedge-agent.rb
+++ Formula/tedge-agent.rb
@@ -6,6 +6,7 @@ class TedgeAgent < Formula
   url "https://raw.githubusercontent.com/thin-edge/homebrew-tedge/main/extras/tedge-agent-logs"
   sha256 "42dec6c7a1b2c53e7c29a4b73ddd7ccf3b2cbb916224d3fe57900a9320aae7bb"
   depends_on "tedge" => :optional
+  depends_on "logrotate" => :recommended
 
   resource "tedge-agent-logs" do
     url "https://raw.githubusercontent.com/thin-edge/homebrew-tedge/main/extras/tedge-agent-logs"
@@ -15,6 +16,24 @@ class TedgeAgent < Formula
   def install
     # log helper
     resource("tedge-agent-logs").stage { bin.install "tedge-agent-logs" }
+
+    # logrotate configuration to prevent large log files
+    logrotate_dir = etc/"logrotate.d"
+    logrotate_conf = logrotate_dir/"tedge-agent.conf"
+
+    if logrotate_dir.exist?
+      if !logrotate_conf.exist?
+        logrotate_conf.write <<~EOS
+          #{var}/log/tedge-agent.log {
+            daily
+            copytruncate
+            rotate 3
+            size 10M
+            compress
+          }
+        EOS
+      end
+    end
   end
 
   service do
